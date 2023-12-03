c8cdc0010463d98518bb57376002429001431eeb
c3rb3ru5d3d53c <c3rb3ru5d3d53c@gmail.com>
2023-12-03 10:10:50 -0400

    comments

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,48 +1,44 @@
 return {
   updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
-    show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
-    remotes = { -- easily add new remotes to track
-      --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
-      --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
-      --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
-    },
+    remote = "origin", 
+    channel = "stable", 
+    version = "latest", 
+    branch = "nightly", 
+    commit = nil, 
+    pin_plugins = nil, 
+    skip_prompts = false, 
+    show_changelog = true, 
+    auto_quit = false, 
+    remotes = {},
   },
 
-  -- Set colorscheme to use
+  
   colorscheme = "astrodark",
 
-  -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+  
   diagnostics = {
     virtual_text = true,
     underline = true,
   },
 
   lsp = {
-    -- customize lsp formatting options
+    
     formatting = {
-      -- control auto formatting on save
+      
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
+        enabled = true, 
+        allow_filetypes = { 
+          
         },
-        ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
+        ignore_filetypes = { 
+          
         },
       },
-      disabled = { -- disable formatting capabilities for the listed language servers
-        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
+      disabled = { 
+        
+        
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 1000, 
     },
     servers = {
       "pyright", "yls", "clangd"
